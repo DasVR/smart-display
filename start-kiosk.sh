127e2ad452363d8200e77ef0f32c48b88c590df0
DasVR <arriqaalraee@gmail.com>
2026-08-29 03:47:18 +0000

    fix kiosk: force HDMI-A-1 to 1920x1080 instead of top-left 1080p-on-4K

---
+++ start-kiosk.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+export XDG_RUNTIME_DIR=/run/user/1000
+export WAYLAND_DISPLAY=wayland-1
+export WLR_BACKENDS=drm,libinput
+export WLR_LIBINPUT_NO_DEVICES=1
+
+/usr/bin/cage -s -- \
+  /usr/bin/chromium-browser \
+  --enable-features=UseOzonePlatform \
+  --ozone-platform=wayland \
+  --kiosk \
+  --app=http://localhost:3000 \
+  --disable-features=TranslateUI \
+  --disable-infobars \
+  --disable-session-crashed-bubble \
+  --disable-restore-session-state \
+  --noerrdialogs \
+  --hide-scrollbars \
+  --autoplay-policy=no-user-gesture-required &
+
+CAGE_PID=$!
+
+# wait for cage to create the wayland socket
+for i in {1..30}; do
+  if [ -S "$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" ]; then
+    sleep 1
+    /usr/bin/wlr-randr --output HDMI-A-1 --mode 1920x1080 || true
+    break
+  fi
+  sleep 0.5
+done
+
+wait $CAGE_PID
